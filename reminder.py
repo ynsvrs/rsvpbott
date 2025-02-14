02f1d6bddafeae49ad787542992f4175cc08fca1
ysnvrs <saniyadair@gmail.com>
2025-02-14 21:41:22 +0500

    rsvp

--- reminder.py
+++ reminder.py
@@ -7,6 +7,6 @@ def schedule_reminder(chat_id, event_name, event_time, bot):
         wait_time = (event_time - datetime.now()).total_seconds() - 3600
         if wait_time > 0:
             time.sleep(wait_time)
-            bot.send_message(chat_id, f"⏰ Reminder: '{event_name}' starts in 1 hour!")
+            bot.send_message(chat_id, f"⏰ Reminder: '{event_name}' starts in 1 hour!!")
     thread = threading.Thread(target=remind)
     thread.start()
